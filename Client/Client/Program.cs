729b3cc1c4b06e2d7bc73f82e31a544b53a7d4ba
Nooheat <nooheat1228@gmail.com>
2017-03-23 23:02:14 +0900

    C# Synchronous Socket 1on1 Chatting

---
+++ Client/Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace Client
+{
+    class Client
+    {
+
+        static void Main(string[] args)
+        {
+            Socket client = null;
+
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                client.Connect(ipep);
+                Console.WriteLine(client.Connected);
+            }
+            catch (Exception e) { Console.WriteLine("Connecting failed"); }
+
+            string s = "hey bro!";
+
+
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            try
+            {
+                while (true)
+                {
+                    client.Send(message);
+                    Thread.Sleep(500); // 안쓰면 오류
+                    client.Send(Encoding.ASCII.GetBytes("!@!"));
+                    Console.WriteLine("Sending Succeed");
+                }
+
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace ConsoleApp1
+{
+    class Server
+    {
+
+        static void Main(string[] args)
+        {
+            byte[] sentMessage = new byte[1024];
+
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Bind(ipep);
+
+                server.Listen(5);
+
+                Socket client = server.Accept();
+
+                while (true)
+                {
+                    int byteRec = client.Receive(sentMessage);
+                    Console.WriteLine(Encoding.ASCII.GetString(sentMessage, 0, byteRec));
+                    byteRec = client.Receive(sentMessage);
+                    Console.WriteLine(Encoding.ASCII.GetString(sentMessage, 0, byteRec));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
